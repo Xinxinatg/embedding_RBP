9e20277143c85784c558154e234207cdf79069fb
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-10-25 13:26:00 +1100

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,22 @@
+# https://www.biostars.org/p/4881/
+import sys
+from Bio import SeqIO
+
+fasta_file = sys.argv[1]  # Input fasta file
+remove_file = sys.argv[2] # Input wanted file, one gene name per line
+result_file = sys.argv[3] # Output fasta file
+
+remove = set()
+with open(remove_file) as f:
+    for line in f:
+        line = line.strip()
+        if line != "" and '>' not in line:
+            remove.add(line)
+
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+
+with open(result_file, "w") as f:
+    for seq in fasta_sequences:
+        nuc = str(seq.seq)
+        if nuc not in remove and len(nuc) > 0:
+            SeqIO.write([seq], f, "fasta")
