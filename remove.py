bd9a398047fe346f5815a8d51419f0c6d7f781a0
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-10-25 13:44:53 +1100

    Update remove.py

--- remove.py
+++ remove.py
@@ -6,13 +6,17 @@ fasta_file = sys.argv[1]  # Input fasta file
 remove_file = sys.argv[2] # Input wanted file, one gene name per line
 result_file = sys.argv[3] # Output fasta file
 
-remove = set()
-with open(remove_file) as f:
-    for line in f:
-        line = line.strip()
-        if line != "" and '>' not in line:
-            remove.add(line)
-
+remove = []
+# with open(remove_file) as f:
+#     for line in f:
+#         line = line.strip()
+#         if line != "" and '>' not in line:
+#             remove.add(line)
+# print(len(remove))
+rm_sequences = SeqIO.parse(open(remove_file),'fasta')
+for rm_seq in rm_sequences:
+    remove.append(str(rm_seq.seq))
+print(len(remove))
 fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
 
 with open(result_file, "w") as f:
